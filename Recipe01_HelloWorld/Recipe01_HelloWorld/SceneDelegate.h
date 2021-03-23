0294efbcf95d8a1d5d8dc560c917621955b5437d
GavinXiang <gavin.xiang@planetart.cn>
2021-03-23 11:45:34 +0800

    [Sample-Code] Recipe01_HelloWorld

---
+++ Recipe01_HelloWorld/Recipe01_HelloWorld/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Recipe01_HelloWorld
+//
+//  Created by MichaelLedger on 2021/3/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Recipe01_HelloWorld/Recipe01_HelloWorld/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Recipe01_HelloWorld
+//
+//  Created by MichaelLedger on 2021/3/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
