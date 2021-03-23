e0c852fcb2f633aeb06de800c9042465375e4fe0
GavinXiang <gavin.xiang@planetart.cn>
2021-03-23 16:10:56 +0800

    [Sample-Code] Recipe02_DisplayingImage

---
+++ Recipe02_DisplayingImage/Recipe02_DisplayingImage/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Recipe02_DisplayingImage
+//
+//  Created by MichaelLedger on 2021/3/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
